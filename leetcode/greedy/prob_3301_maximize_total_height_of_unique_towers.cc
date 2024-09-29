530d1d630718d1f78abb7d88af2926ecee061167
sanjibkd <sanjib.kgp@gmail.com>
2024-09-29 16:48:27 -0700

    Update prob_3301_maximize_total_height_of_unique_towers.cc

--- leetcode/greedy/prob_3301_maximize_total_height_of_unique_towers.cc
+++ leetcode/greedy/prob_3301_maximize_total_height_of_unique_towers.cc
@@ -1,1 +1,17 @@
-
+class Solution {
+  public:
+    long long maximumTotalSum(vector<int>& maximumHeight) {
+      long long res = 0;
+      sort(maximumHeight.begin(), maximumHeight.end(), greater<int>());
+      int n = maximumHeight.size();
+      if (maximumHeight[0] < n) return -1;
+      int x = INT_MAX;
+      for (int mh : maximumHeight) {
+        x = min(x, mh);
+        if (x == 0) return -1;
+        res += x;
+        --x;
+      }
+      return res;
+    }
+};
